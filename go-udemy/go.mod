329ab7a053d212e810d1e2a0b20a6d640a67497c
Alfan Herya <alfanherya@gmail.com>
2021-11-17 23:00:02 +0700

    feature-goudemy

---
+++ go-udemy/go.mod
@@ -0,0 +1,5 @@
+module github.com/alfanherya/go-workspace/go-udemy
+
+go 1.16
+
+require golang.org/x/text v0.3.7
---
+++ go-udemy/goroutine_test.go
@@ -0,0 +1,32 @@
+package goudemy
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func TestCreateGoroutine(t *testing.T) {
+	// untuk membuat goroutine cukup tambahkan go di awal func yang akan di running sebagai
+	// go routine, karena go routine adalah sebuah proses async.
+	go RunHelloWorld()
+	fmt.Println("Ups")
+
+	time.Sleep(1 * time.Second)
+}
+
+func DisplayNumber(number int) {
+	fmt.Println("Display", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(10 * time.Second)
+}
